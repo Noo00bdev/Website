2b5db3ba4257a87f904af92e6601fe5d4e1a567a
NINTCH <135129862+Noo00bdev@users.noreply.github.com>
2025-07-04 20:14:40 +0000

    icon changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 
 let menuList = document.getElementById("menuList");
+let icon = document.querySelector(".menu-icon-img")
 
 function toggleMenu(){
-    menuList.classList.toggle("open");       
+    menuList.classList.toggle("open");
+    if (icon.getAttribute('src').includes('menu.png')) {
+        icon.setAttribute('src', 'Logo/close.png');
+    } else {
+        icon.setAttribute('src', 'Logo/menu.png');
+    }   
 }
